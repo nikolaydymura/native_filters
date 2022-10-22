7e0c1c68b931d7d033eaad5f056eb16dafb0d887
Nikolay Dymura <nikolay.dymura@gmail.com>
2022-10-22 09:54:38 +0300

    Added swipe filter for demo

--- example/lib/pages/filter_details.dart
+++ example/lib/pages/filter_details.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:native_filters/native_filters.dart';
+import 'package:image/image.dart' as img;
 
 import 'filter_preview.dart';
 import 'filter_result.dart';
@@ -19,11 +23,9 @@ class FilterDetailsScreen extends StatefulWidget {
 }
 
 class _FilterDetailsState extends State<FilterDetailsScreen> {
-  Filter? _filter;
+  Filterable? _filter;
 
   List<FilterInput> _details = [];
-
-  final List<String> _platformPreviews = ['Image Preview', 'Video Preview'];
 
   @override
   void initState() {
@@ -83,8 +85,7 @@ class _FilterDetailsState extends State<FilterDetailsScreen> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) =>
-                        FilterPreviewScreen(filter: _filter!),
+                    builder: (context) => FilterPreviewScreen(filter: _filter!),
                   ),
                 );
               },
@@ -104,7 +105,49 @@ class _FilterDetailsState extends State<FilterDetailsScreen> {
   }
 
   Future<void> _loadFilterInfo() async {
-    _filter = await widget.factory.createFilter(widget.filter.name);
+    if (Platform.isIOS) {
+      final filterGroup = await widget.factory.createFilterGroup();
+      final filter = await filterGroup.addFilter(widget.filter.name);
+      if (widget.filter.name == 'CIColorCube') {
+        const size = 64;
+        await filter?.setNumValue('inputCubeDimension', size);
+
+        ByteData data = await rootBundle.load('filters/filter_lut_3.png');
+        Uint8List bytes = data.buffer.asUint8List();
+        img.Image? photo = img.decodeImage(bytes);
+        final bitmap = photo!.getBytes(format: img.Format.argb);
+        final lutData = lutPngToNSData(64, bitmap, photo.width, photo.height);
+
+        ByteData data1 = await rootBundle.load('filters/filter_lut_3.data');
+        Uint8List bytes1 = data.buffer.asUint8List();
+        //for(var i =0; i< lutData.length; i ++){
+        //print('i=$i, ${lutData[i]}==${bytes1[i]} = ${lutData[i] == bytes1[i]}');
+        //}
+
+        await filter?.setNSData('inputCubeData', lutData);
+      }
+      final previewFilter = await filterGroup.addFilter('CISwipeTransition');
+      print(previewFilter?.index);
+      await previewFilter?.setNumValue('inputTime', 0.5);
+      await previewFilter?.setCIImageAsset(
+        'inputTargetImage',
+        'images/test.jpg',
+      );
+      await previewFilter?.setNumValue('inputWidth', 0.0);
+      await previewFilter
+          ?.setDoubleArrayValue('inputExtent', [0.0, 0.0, 1200.0, 1788.0]);
+      await previewFilter?.setColorValue('inputColor', Colors.black);
+      _filter = filterGroup;
+    } else {
+      final filter = await widget.factory.createFilter(widget.filter.name);
+      if (widget.filter.name == 'GlLookUpTableFilter') {
+        await filter.setBitmapAsset(
+          'inputCubeData',
+          'filters/lookup_sample.png',
+        );
+      }
+      _filter = filter;
+    }
     _details = FilterFactory.filterAttributes(filterName: widget.filter.name)
             ?.toList() ??
         [];
@@ -132,9 +175,6 @@ class _FilterDetailsState extends State<FilterDetailsScreen> {
         ),
       );
       final data = input.data;
-      if (data['AttributeClass'] == 'num') {
-        items.add(_NumField(name: input.name, filter: _filter!, attribute: data));
-      }
       items.addAll(_attributeDetails(data));
     }
     return items;
