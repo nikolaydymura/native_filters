841efa1b7a2d78073eceb8f362c7fcb588c7a82a
EgorEko <asya.wear@gmail.com>
2022-10-24 12:03:14 +0300

    Changed FavoritesPage

--- example/lib/pages/favourites_page.dart
+++ example/lib/pages/favourites_page.dart
@@ -5,37 +5,25 @@ class FavoritesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      initialIndex: 0,
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Center(child: Text('favorites')),
-          bottom: const TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(
-                  Icons.monochrome_photos,
-                ),
-              ),
-              Tab(
-                icon: Icon(
-                  Icons.loupe,
-                ),
-              ),
-              Tab(
-                icon: Icon(Icons.invert_colors),
-              ),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('Favorites')),
+      ),
+      body: ListView(
+        children: const <Widget>[
+          SizedBox(
+            height: 50,
+            child: Center(child: Text('Monochrome')),
           ),
-        ),
-        body: const TabBarView(
-          children: <Widget>[
-            Center(child: Text('Monochrome')),
-            Center(child: Text('Lookuptable')),
-            Center(child: Text('ColorCube there')),
-          ],
-        ),
+          SizedBox(
+            height: 50,
+            child: Center(child: Text('Lookuptable')),
+          ),
+          SizedBox(
+            height: 50,
+            child: Center(child: Text('ColorCube ')),
+          ),
+        ],
       ),
     );
   }
