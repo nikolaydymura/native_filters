0ecf1351c0166c8c3d94d56b19424bd1bb6452c2
Nikolay Dymura <nikolay.dymura@gmail.com>
2022-11-16 13:05:40 +0200

    Added looping for video player

--- ios/Classes/VideoPreview.swift
+++ ios/Classes/VideoPreview.swift
@@ -25,10 +25,22 @@ class VideoPreview: NSObject, FlutterTexture {
     
     override init() {
         player.actionAtItemEnd = .none
+        super.init()
+        NotificationCenter.default.addObserver(self, selector: #selector(playerItemDidReachEnd(notification: )), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
+    }
+    
+    
+    @objc func playerItemDidReachEnd(notification: Notification) {
+        player.seek(to: CMTime.zero)
+        player.play()
     }
     
     func copyPixelBuffer() -> Unmanaged<CVPixelBuffer>? {
-       let outputItemTime = videoOutput.itemTime(forHostTime: CACurrentMediaTime())
+        let outputItemTime = videoOutput.itemTime(forHostTime: CACurrentMediaTime())
         if videoOutput.hasNewPixelBuffer(forItemTime: outputItemTime) {
             if let buffer = videoOutput.copyPixelBuffer(forItemTime: outputItemTime, itemTimeForDisplay: nil) {
                 return Unmanaged.passRetained(buffer)
@@ -42,7 +54,7 @@ class VideoPreview: NSObject, FlutterTexture {
     }
     
     func setSource(url: URL) {
-    
+        
         videoOutput = AVPlayerItemVideoOutput(pixelBufferAttributes: [
             kCVPixelBufferPixelFormatTypeKey as String: kCVPixelFormatType_32BGRA,
             kCVPixelBufferIOSurfacePropertiesKey as String : [:] ])
@@ -133,13 +145,13 @@ class VideoPreview1: NSObject, FlutterPlatformView {
     let methodChannel: FlutterMethodChannel
     let registrar: FlutterPluginRegistrar
     let filtersFactory: ImageVideoFilterFactory
-
+    
     var playerViewController:AVPlayerViewController?
-
+    
     private var player: AVPlayer?
-
+    
     var selectedFilter: NativeFilter?
-
+    
     init(frame: CGRect, registrar: FlutterPluginRegistrar, id: Int64, factory: ImageVideoFilterFactory) {
         methodChannel = FlutterMethodChannel(
             name: "FilterVideoPreview_\(id)",
@@ -147,31 +159,31 @@ class VideoPreview1: NSObject, FlutterPlatformView {
         )
         player = AVPlayer()
         player?.actionAtItemEnd = .none
-
+        
         self.playerViewController = AVPlayerViewController()
         if #available(iOS 10.0, *) {
             self.playerViewController?.updatesNowPlayingInfoCenter = false
         }
-
+        
         self.playerViewController?.player = player
         self.playerViewController?.view.frame = frame
         self.playerViewController?.showsPlaybackControls = true
-
+        
         let viewController = (UIApplication.shared.delegate?.window??.rootViewController)!
         viewController.addChild(self.playerViewController!)
-
+        
         filtersFactory = factory
         self.registrar = registrar
         super.init()
         methodChannel.setMethodCallHandler(callHandler)
     }
-
+    
     func callHandler(call: FlutterMethodCall, result: @escaping FlutterResult) {
         if call.method == "setFilter" {
             guard let index = call.arguments as? Int else {
                 return result(FlutterError.init())
             }
-
+            
             result(nil)
         } else if call.method == "update" {
             guard let item = selectedFilter?.processingVideo else {
@@ -189,7 +201,7 @@ class VideoPreview1: NSObject, FlutterPlatformView {
             result(nil)
         }
     }
-
+    
     func view() -> UIView {
         return playerViewController!.view
     }
@@ -199,12 +211,12 @@ public class FilterVideoPreviewFactory : NSObject, FlutterPlatformViewFactory {
     let registrar: FlutterPluginRegistrar
     let factory: ImageVideoFilterFactory
     let id: String = "FilterVideoPreview"
-
+    
     init(registrar: FlutterPluginRegistrar, factory: ImageVideoFilterFactory) {
         self.registrar = registrar
         self.factory = factory
     }
-
+    
     public func create(
         withFrame frame: CGRect,
         viewIdentifier viewId: Int64,
